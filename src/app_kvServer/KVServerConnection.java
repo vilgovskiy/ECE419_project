f346fbae757ff56094750544ad40df82dd06776a
vilgovskiy <43548101+vilgovskiy@users.noreply.github.com>
2019-01-20 22:46:12 -0500

    connection run implementation

--- src/app_kvServer/KVServerConnection.java
+++ src/app_kvServer/KVServerConnection.java
@@ -1,7 +1,9 @@
 package app_kvServer;
 
 import org.apache.log4j.Logger;
+import server.TextMessage;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -19,6 +21,7 @@ public class KVServerConnection implements Runnable {
 
     /**
      * Constructs a new CientConnection object for a given TCP socket.
+     *
      * @param clientSocket the Socket object for the client connection.
      */
     public KVServerConnection(Socket clientSocket) {
@@ -28,6 +31,36 @@ public class KVServerConnection implements Runnable {
 
     @Override
     public void run() {
-        
+        try {
+            output = clientSocket.getOutputStream();
+            input = clientSocket.getInputStream();
+
+            while (isOpen) {
+//                try {
+//
+//
+//                    /* connection either terminated by the client or lost due to
+//                     * network problems*/
+//                } catch (IOException ioe) {
+//                    logger.error("Error! Connection lost!");
+//                    isOpen = false;
+//                }
+            }
+
+        } catch (IOException ioe) {
+            logger.error("Error! Connection could not be established!", ioe);
+
+        } finally {
+
+            try {
+                if (clientSocket != null) {
+                    input.close();
+                    output.close();
+                    clientSocket.close();
+                }
+            } catch (IOException ioe) {
+                logger.error("Error! Unable to tear down connection!", ioe);
+            }
+        }
     }
 }
