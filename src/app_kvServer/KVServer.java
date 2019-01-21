97bb450911d1030cc23b7bdedd985cea63516c63
vilgovskiy <43548101+vilgovskiy@users.noreply.github.com>
2019-01-20 21:14:57 -0500

    KVServer constructor

--- src/app_kvServer/KVServer.java
+++ src/app_kvServer/KVServer.java
@@ -1,89 +1,109 @@
 package app_kvServer;
 
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class KVServer implements IKVServer {
-	/**
-	 * Start KV Server at given port
-	 * @param port given port for storage server to operate
-	 * @param cacheSize specifies how many key-value pairs the server is allowed
-	 *           to keep in-memory
-	 * @param strategy specifies the cache replacement strategy in case the cache
-	 *           is full and there is a GET- or PUT-request on a key that is
-	 *           currently not contained in the cache. Options are "FIFO", "LRU",
-	 *           and "LFU".
-	 */
-	public KVServer(int port, int cacheSize, String strategy) {
-		// TODO Auto-generated method stub
-	}
-	
-	@Override
-	public int getPort(){
-		// TODO Auto-generated method stub
-		return -1;
-	}
 
-	@Override
-    public String getHostname(){
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private static Logger logger = Logger.getRootLogger();
 
-	@Override
-    public CacheStrategy getCacheStrategy(){
-		// TODO Auto-generated method stub
-		return IKVServer.CacheStrategy.None;
-	}
+    private int port;
+    private int cacheSize;
+    private String strategy;
+    private boolean running;
+    private ServerSocket serverSocket;
 
-	@Override
-    public int getCacheSize(){
-		// TODO Auto-generated method stub
-		return -1;
-	}
+    /**
+     * Start KV Server at given port
+     *
+     * @param port      given port for storage server to operate
+     * @param cacheSize specifies how many key-value pairs the server is allowed
+     *                  to keep in-memory
+     * @param strategy  specifies the cache replacement strategy in case the cache
+     *                  is full and there is a GET- or PUT-request on a key that is
+     *                  currently not contained in the cache. Options are "FIFO", "LRU",
+     *                  and "LFU".
+     */
 
-	@Override
-    public boolean inStorage(String key){
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public KVServer(int port, int cacheSize, String strategy) {
+        this.port = port;
+        this.cacheSize = cacheSize;
+        this.strategy = strategy;
+        logger.info("Creating an instance of the KV server");
+    }
 
-	@Override
-    public boolean inCache(String key){
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public int getPort() {
+        // TODO Auto-generated method stub
+        return -1;
+    }
 
-	@Override
-    public String getKV(String key) throws Exception{
-		// TODO Auto-generated method stub
-		return "";
-	}
+    @Override
+    public String getHostname() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-    public void putKV(String key, String value) throws Exception{
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public CacheStrategy getCacheStrategy() {
+        // TODO Auto-generated method stub
+        return IKVServer.CacheStrategy.None;
+    }
 
-	@Override
-    public void clearCache(){
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public int getCacheSize() {
+        // TODO Auto-generated method stub
+        return -1;
+    }
 
-	@Override
-    public void clearStorage(){
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public boolean inStorage(String key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-    public void run(){
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public boolean inCache(String key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-    public void kill(){
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public String getKV(String key) throws Exception {
+        // TODO Auto-generated method stub
+        return "";
+    }
 
-	@Override
-    public void close(){
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void putKV(String key, String value) throws Exception {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void clearCache() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void clearStorage() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void kill() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+    }
 }
