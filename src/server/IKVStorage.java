6061ab274db2c7ea40b22b791ebe20915191fdd5
vilgovskiy <43548101+vilgovskiy@users.noreply.github.com>
2019-01-22 15:34:56 -0500

    kvstorage put file to server

--- src/server/IKVStorage.java
+++ src/server/IKVStorage.java
@@ -1,4 +1,11 @@
 package server;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
 public interface IKVStorage {
+    String storagePath = ".//storage//";
+
+    public void writeToDisk (String key, String value) throws FileNotFoundException, UnsupportedEncodingException;
+
 }
--- src/server/KVStorage.java
+++ src/server/KVStorage.java
@@ -1,4 +1,15 @@
 package server;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 public class KVStorage implements IKVStorage {
+    @Override
+    public void writeToDisk(String key, String value) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter(storagePath + key + ".txt", "UTF-8");
+        writer.write(value);
+    }
+
+
 }
