a93733b9fcf4b9e1cb6ed63619e538edf815c894
vilgovskiy <43548101+vilgovskiy@users.noreply.github.com>
2019-02-16 22:01:53 -0500

    Add to ecs ring

--- src/ecs/ECSConsistentHash.java
+++ src/ecs/ECSConsistentHash.java
@@ -1,4 +1,22 @@
 package ecs;
 
+import org.apache.log4j.Logger;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class ECSConsistentHash {
+
+    private final SortedMap<String, ECSNode> ring = new TreeMap<>();
+
+    private Logger logger = Logger.getRootLogger();
+
+    public ECSConsistentHash(){
+    }
+
+    public void addNode(ECSNode node){
+        String nodeHash = node.getNodeHash();
+        ring.put(nodeHash, node);
+    }
+
 }
--- src/testing/ECStests.java
+++ src/testing/ECStests.java
@@ -1,14 +1,29 @@
 package testing;
 
+import ecs.ECSConsistentHash;
 import ecs.ECSNode;
 import junit.framework.TestCase;
 import org.junit.Test;
 
 public class ECStests extends TestCase {
 
+
     @Test
     public void testECSNodeCreation(){
         ECSNode node = new ECSNode("node_name", "localhost", 50000);
+    }
+
+    @Test
+    public void testECSRingInsert(){
+
+        ECSConsistentHash hashRing = new ECSConsistentHash();
+        ECSNode node = new ECSNode("node_name", "localhost", 50000);
+        ECSNode node1 = new ECSNode("node_name", "google.com", 50001);
+        ECSNode node2 = new ECSNode("node_name", "yandex", 50543);
+
+        hashRing.addNode(node);
+        hashRing.addNode(node1);
+        hashRing.addNode(node2);
     }
 
 }
