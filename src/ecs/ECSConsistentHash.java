a0abb18ba48f9a3a6ebb29ab66fde363f40bd4d9
vilgovskiy <43548101+vilgovskiy@users.noreply.github.com>
2019-02-16 22:19:06 -0500

    Remove impl for hash ring

--- src/ecs/ECSConsistentHash.java
+++ src/ecs/ECSConsistentHash.java
@@ -1,5 +1,6 @@
 package ecs;
 
+import com.google.gson.Gson;
 import org.apache.log4j.Logger;
 
 import java.util.SortedMap;
@@ -17,6 +18,28 @@ public class ECSConsistentHash {
     public void addNode(ECSNode node){
         String nodeHash = node.getNodeHash();
         ring.put(nodeHash, node);
+
+        String prevNodeKey =  ring.headMap(nodeHash).lastKey();
+        ECSNode prevNode = ring.get(prevNodeKey);
+
     }
+
+    public ECSNode removeNode(String key){
+        return  ring.remove(key);
+    }
+
+    public void removeAllNodes(){
+        ring.clear();
+    }
+
+    public boolean empty(){
+        return ring.isEmpty();
+    }
+
+    public String serializeHash (){
+        return "";
+    }
+
+
 
 }
